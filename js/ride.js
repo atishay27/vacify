8e730d9436ea704e68515dce396aae10a380988b
atishay27 <48438744+atishay27@users.noreply.github.com>
2021-05-04 23:36:56 +0530

    testing with python

--- js/ride.js
+++ js/ride.js
@@ -22,3 +22,27 @@ function requestUnicorn() {
 function completeRequest(result) {
     console.log('Response received from API: ', result);
 }
+function requestUnicorn2() {
+    $.ajax({
+        method: 'POST',
+        url: 'https://kwuk5atz5l.execute-api.ap-south-1.amazonaws.com/prod/ride',
+        headers: {
+        },
+        data: JSON.stringify({
+            PickupLocation: {
+                Latitude: 12,
+                Longitude: 77
+            }
+        }),
+        contentType: 'application/json',
+        success: completeRequest,
+        error: function ajaxError(jqXHR, textStatus, errorThrown) {
+            console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
+            console.error('Response: ', jqXHR.responseText);
+            alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+        }
+    });
+}
+function completeRequest(result) {
+    console.log('Response received from API: ', result);
+}
